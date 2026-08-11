c9fdd49bb3e2c4e0fe67be29e84b74fc928097bb
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-11 20:17:30 +1000

    0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module defines index_range"""
 
+
 from typing import Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """This function paginates, returns a start and end indexes related to range of indexes"""
