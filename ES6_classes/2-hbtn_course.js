3e1795c7d8011fa7d0d077dd784b983707ed4e3a
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-19 14:51:47 +1000

    saving progress

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -3,14 +3,32 @@ class HolbertonCourse {
         this._name = name
         this._length = length
         this._students = students
+
+
+
+    }
+    get name() {
+        return this._name
+    }
+    set name() {
         if (typeof name != String) {
-            throw new TypeError('"Name" must be a string')
-        }
+        throw new TypeError('"Name" must be a string')
+        }   
+    }
+    get length() {
+        return this._length
+    }
+    set length() {
         if (typeof length != Number) {
-            throw new TypeError('"length" must be a number')
-        }
-        if (typeof students != Array) {
-            throw new TypeError('"students" must be a array')
-        }
+        throw new TypeError('"length" must be a number')
+    }
+    }
+    get students() {
+        return this._students
+    }
+    set students() {
+        if (typeof students != Array(String)) {
+        throw new TypeError('"students" must be a array')
+    }
     }
 }
