b9f93041ba2526482778468be2d4bb310dc07ebb
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-28 20:07:38 +1000

    5-typed_arrays.js

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value){
+const buffer = new ArrayBuffer(length);
+    if (position < 0 || position >= length) {
+        throw new Error('Position outside range');
+    }
+    const data = new DataView(buffer);
+    data.setInt8(position, value);
+    return data;
+}
