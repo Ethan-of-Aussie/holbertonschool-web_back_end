67ab962fc1ec7e60a2ae9430c60368854f6bc5c8
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-30 16:38:32 +1000

    1-promise.js

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,9 @@
+import { error } from "node:console";
+
 export default function getFullResponseFromAPI(success){
     if (success){
         return Promise.resolve({status : 200, body : 'Success'});
     } else {
-        return Promise.reject('The fake API is not working currently');
+        return Promise.reject(new Error('The fake API is not working currently'));
     }
 }
