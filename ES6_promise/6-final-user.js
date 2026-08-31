83c251ea458e4ba55b61ead2aab0350d7547065c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-31 14:16:52 +1000

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -9,7 +9,7 @@ export default function handleProfileSignup(firstName, lastName, fileName){
     .then((result) => result.map((item) => (
     {
       status: item.status,
-      value: item.value.toString(),
+      value: item.value,
     })
   )
 )
