0462b43dba09897e5787148a88b6a28896a18155
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-09-01 22:15:03 +1000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 const readline = require('node:readline');
+
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input)=> {
-    console.log(`Your name is: ${input}`);
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
 });
-rl.on('close', () => {console.log('This important software is now closing');});
+rl.on('close', () => { console.log('This important software is now closing'); });
