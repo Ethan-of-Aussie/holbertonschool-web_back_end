9e5bbe84a7958d50ce0141630084cc6031ffd455
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-09-02 12:50:01 +1000

    2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -13,13 +13,16 @@ function countStudents(file) {
     const students = lines.slice(1);
     console.log(`Number of students: ${students.length}`);
     for (const student of students) {
-      const [firstname, lastname, age, field] = student.split(',');
-      if (field === 'CS') {
-        cs.push(firstname);
-      }
-      if (field === 'SWE') {
-        swe.push(firstname);
-      }
+        const parts = student.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
+        
+        if (field === 'CS') {
+         cs.push(firstname);
+        }
+        if (field === 'SWE') {
+          swe.push(firstname);
+             }
     }
     console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}
 Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
