9e04b0a879084ecfefd0402c8784aab9f59e4f95
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-09-03 15:43:42 +1000

    3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -5,27 +5,31 @@ async function countStudents(file) {
   const cs = [];
   const swe = [];
 
-  await fs.readFile(`${file}`, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
-    console.log(`Number of students: ${students.length}`);
-    for (const student of students) {
-      const parts = student.split(',');
-      const firstname = parts[0];
-      const field = parts[3];
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${file}`, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      console.log(`Number of students: ${students.length}`);
+      for (const student of students) {
+        const parts = student.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
 
-      if (field === 'CS') {
-        cs.push(firstname);
+        if (field === 'CS') {
+          cs.push(firstname);
+        }
+        if (field === 'SWE') {
+          swe.push(firstname);
+        }
       }
-      if (field === 'SWE') {
-        swe.push(firstname);
-      }
-    }
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}
 Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+      resolve();
+    });
   });
 }
 module.exports = countStudents;
