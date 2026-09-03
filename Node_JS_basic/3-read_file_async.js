f466f30017caf23c394e1b22c34e809f8727c186
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-09-03 15:37:49 +1000

    3-read_file_async.js

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+
+async function countStudents(file) {
+  /* use process.argv, */
+  const cs = [];
+  const swe = [];
+
+  await fs.readFile(`${file}`, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+    console.log(`Number of students: ${students.length}`);
+    for (const student of students) {
+      const parts = student.split(',');
+      const firstname = parts[0];
+      const field = parts[3];
+
+      if (field === 'CS') {
+        cs.push(firstname);
+      }
+      if (field === 'SWE') {
+        swe.push(firstname);
+      }
+    }
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}
+Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  });
+}
+module.exports = countStudents;
